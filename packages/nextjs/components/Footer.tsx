98d6188723535052ee8f3816511f81032ca07f84
Juan Manuel <ethkipu@gmail.com>
2024-10-14 22:27:29 +0200

    reCaptcha for validate form implemented

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -1,22 +1,9 @@
-import React from "react";
-// import Link from "next/link";
-import { hardhat } from "viem/chains";
-// import { CurrencyDollarIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-// import { HeartIcon } from "@heroicons/react/24/outline";
-import { SwitchTheme } from "~~/components/SwitchTheme";
-// import { BuidlGuidlLogo } from "~~/components/assets/BuidlGuidlLogo";
-// import { Faucet } from "~~/components/scaffold-eth";
-import { useTargetNetwork } from "~~/hooks/scaffold-eth/useTargetNetwork";
-
-// import { useGlobalState } from "~~/services/store/store";
-
 /**
  * Site footer
  */
 export const Footer = () => {
   // const nativeCurrencyPrice = useGlobalState(state => state.nativeCurrency.price);
-  const { targetNetwork } = useTargetNetwork();
-  const isLocalNetwork = targetNetwork.id === hardhat.id;
+  // const { targetNetwork } = useTargetNetwork();
 
   return (
     <div className="min-h-0 px-1 py-5 mb-11 lg:mb-0">
@@ -42,7 +29,7 @@ export const Footer = () => {
               </>
             )}
           </div> */}
-          <SwitchTheme className={`pointer-events-auto ${isLocalNetwork ? "self-end md:self-auto" : ""}`} />
+          {/* <SwitchTheme className={`pointer-events-auto ${isLocalNetwork ? "self-end md:self-auto" : ""}`} /> */}
         </div>
       </div>
       <div className="w-full">
