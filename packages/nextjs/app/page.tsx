d1d14fcb9da1cff9acfcd760e39583fb134cef6c
Ruben Santana <rubensantana@outlook.com>
2024-11-21 18:54:17 -0400

    hardcode the tx gas

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -113,6 +113,7 @@ const Home: NextPage = () => {
       functionName: "requestWithdraw",
       args: [inputAddress.trim(), parseEther(dailyLimitValue)],
       account: accounts[indexAccount],
+      gas: 150000n,
     });
   };
 
