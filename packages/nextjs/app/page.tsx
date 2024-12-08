3477af76da18a7e2e64a3fb08829ef4d0167da2f
Ruben Santana <39445525+santanaruben@users.noreply.github.com>
2024-12-08 14:30:02 -0400

    Update page.tsx

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -2,9 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Image from "next/image";
-import dayjs from "dayjs";
 import "dayjs/locale/es";
-import relativeTime from "dayjs/plugin/relativeTime";
 import type { NextPage } from "next";
 import { useTheme } from "next-themes";
 // import ReCAPTCHA from "react-google-recaptcha";
@@ -23,18 +21,17 @@ import { privateKeyToAccount } from "viem/accounts";
 //   scrollSepolia,
 //   sepolia
 // } from "viem/chains";
-import { useAccount, usePublicClient, useWriteContract } from "wagmi";
+import { usePublicClient, useWriteContract } from "wagmi";
 import { ChangeChain } from "~~/components/ChangeChain";
 // import { WriteContractReturnType } from "wagmi/actions";
 import { AddressInput } from "~~/components/scaffold-eth";
 import {
-  useScaffoldEventHistory,
   useScaffoldReadContract,
   useTargetNetwork,
   useTransactor,
 } from "~~/hooks/scaffold-eth";
 import { useScaffoldContract } from "~~/hooks/scaffold-eth/useScaffoldContract";
-import { getAlchemyHttpUrl, getBlockExplorerTxLink } from "~~/utils/scaffold-eth";
+import { getAlchemyHttpUrl } from "~~/utils/scaffold-eth";
 
 // import { useGlobalState } from "~~/services/store/store";
 // import { getTargetNetworks } from "~~/utils/scaffold-eth/networks";
@@ -63,8 +60,6 @@ const Home: NextPage = () => {
 
   const [loading, setLoading] = useState(false);
   const [inputAddress, setInputAddress]: any = useState();
-
-  const { address: connectedAddress } = useAccount();
 
   const { targetNetwork } = useTargetNetwork();
 
@@ -114,8 +109,8 @@ const Home: NextPage = () => {
       functionName: "requestWithdraw",
       args: [inputAddress.trim(), parseEther(dailyLimitValue)],
       account: accounts[indexAccount],
-      gas: 1000000n,
-      gasPrice: parseGwei("7"),
+      gas: 800000n,
+      gasPrice: parseGwei("7")
     });
   };
 
@@ -139,27 +134,6 @@ const Home: NextPage = () => {
     }
   };
 
-  const {
-    data: withdrawalEvents,
-    isLoading: isLoadingEvents,
-    error: errorReadingEvents,
-    refetch: refetchWithdrawalEvents,
-  } = useScaffoldEventHistory({
-    contractName: "Faucet",
-    eventName: "Withdrawal",
-    fromBlock: 6951976n, // scrollSepolia
-    // fromBlock: 0n, // hardhat
-    blockData: true,
-    watch: true,
-    filters: { _user: connectedAddress ?? "" },
-  });
-
-  useEffect(() => {
-    refetchWithdrawalEvents();
-  }, [connectedAddress]);
-
-  dayjs.extend(relativeTime);
-  dayjs.locale("es");
 
   // const recaptchaRef = useRef<ReCAPTCHA>(null);
   // const [isVerified, setIsVerified] = useState(false);
@@ -255,53 +229,6 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      {isLoadingEvents ? (
-        <div className="mt-8 text-xl font-bold text-center">Cargando Transacciones...</div>
-      ) : (
-        <div className="flex flex-col justify-center flex-grow w-full mt-6 mb-4 md:w-5/6 lg:w-4/5">
-          <div className="my-2 text-xl font-bold text-center">
-            {connectedAddress ? "Transacciones a tu cuenta" : "Últimas Transacciones"}
-          </div>
-          <div className="overflow-x-auto overflow-y-auto border-2 shadow-2xl rounded-xl border-primary max-h-60">
-            <table className="table w-full bg-base-100 table-zebra">
-              <thead className="text-primary font-black bg-[#F2F4FF]">
-                <tr>
-                  <th>n°</th>
-                  <th>Tx</th>
-                  <th>Monto</th>
-                  <th>Cuando</th>
-                </tr>
-              </thead>
-              <tbody>
-                {withdrawalEvents &&
-                  withdrawalEvents.map((event: any, index: number) => {
-                    return (
-                      <tr key={index}>
-                        <th>{withdrawalEvents?.length - index}</th>
-
-                        <td className="text-blue-500">
-                          <a
-                            href={getBlockExplorerTxLink(targetNetwork.id, event?.transactionHash)}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            <span className="lg:hidden" title={event?.transactionHash}>
-                              {event?.transactionHash.slice(0, 6)}...{event?.transactionHash.slice(-4)}
-                            </span>
-                            <span className="hidden lg:table-cell">{event?.transactionHash}</span>
-                          </a>
-                        </td>
-                        <td>{formatEther(event.args[1])} ETH</td>
-                        <td>{dayjs.unix((event?.blockData?.timestamp).toString()).fromNow()}</td>
-                      </tr>
-                    );
-                  })}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      )}
-      {errorReadingEvents && <div className="mt-2 text-red-500">loading...</div>}
     </div>
   );
 };
